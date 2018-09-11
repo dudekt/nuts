6f45f294f7241dabbeab0517216a1388e7a12bfe
Tadeusz Dudek <tadeusz.dudek@vml.com>
2018-09-11 15:31:13 +0200

    Update.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import imagesLoaded from 'imagesloaded';
 import 'sass/styles.scss';
 
 import Msnry from './js/masonry';
-import BagietaConfig from './js/baguette';
+import BagietaConfig, { clickInDot } from './js/baguette';
 
 const elem = document.querySelector('.grid');
 
@@ -12,7 +12,7 @@ imagesLoaded(elem, () => {
   Msnry(elem);
 
   // const animation = ['animated', 'zoomIn'];
-  // const item = document.querySelectorAll('.grid-item');
+  const item = document.querySelectorAll('.grid-item');
   // for (let i = 0; i < item.length; i += 1) {
   //   const duaration = 55;
   //   // item[i].classList.add(...animation);
@@ -20,6 +20,19 @@ imagesLoaded(elem, () => {
   //   // item[i].style.animationDelay = `${duaration * i}ms`;
   //   // item[i].style.animationDuration = '250ms';
   // }
+
+  const aside = document.querySelector('.dots');
+
+  for (let i = 0; i < item.length; i += 1) {
+    const dot = document.createElement('li');
+    dot.classList.add('dot');
+    dot.id = i;
+    aside.appendChild(dot);
+
+    document.getElementById(i).onclick = function clicked() {
+      clickInDot(i);
+    };
+  }
 
   BagietaConfig();
 });
--- src/js/baguette.js
+++ src/js/baguette.js
@@ -1,13 +1,18 @@
 import baguetteBox from 'baguettebox.js';
 
+const gallery = baguetteBox.run('.grid');
+
 const BagietaConfig = () => {
   baguetteBox.run('.grid', {
     animation: 'fadeIn',
     noScrollbars: true,
-    // buttons: false,
     captions: false,
     overlayBackgroundColor: 'rgba(255, 255, 255, 1)',
   });
+};
+
+export const clickInDot = (index) => {
+  baguetteBox.show(index, gallery);
 };
 
 export default BagietaConfig;
